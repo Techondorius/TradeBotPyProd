585711373d0e806ae2d06ed08bb705bdcc6e0426
Techondorius <62565667+Techondorius@users.noreply.github.com>
2020-03-24 04:28:51 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ secret = '***************************************'
 positionsize = 100
 lostcut = 2.6
 
-discord = 'https://discordapp.com/api/webhooks/691693631332941944/a3f6SvEoYLvYuGIbyjEJFWsSPdVoeiWbmaBGtug0QZszblSQv0Tl4_HUmMmMsIUQMzig'
+discord = 'https://discordapp.com/api/webhooks/*************************************************************************'
