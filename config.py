5d465487c81dfe793cca08fb84fdff8bdbbc4e04
Techondorius <kurofuku01@gmail.com>
2020-03-24 14:06:35 +0900

    no message

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-apikey = '****************'
-secret = '***************************************'
+apikey = '4pgicL_PhNHO5_1_TLyHWuLC'
+secret = '2hpboMkWiGMMfnbpAKjQ5OxwW6VsS1-7JwgGhO0ZiA8kbWll'
 
 positionsize = 100
 lostcut = 2.6
 
-discord = 'https://discordapp.com/api/webhooks/*************************************************************************'
+discord = 'https://discordapp.com/api/webhooks/691693631332941944/a3f6SvEoYLvYuGIbyjEJFWsSPdVoeiWbmaBGtug0QZszblSQv0Tl4_HUmMmMsIUQMzig'
